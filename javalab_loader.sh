939ea8cd9ce85b7105d1518dbf7fb1659945187a
Wahlroos Kristian M <wakr@da3-cs-dk108-01.helsinki.fi>
2016-02-24 09:31:36 +0200

    now creates folder structure for repos

--- javalab_loader.sh
+++ javalab_loader.sh
@@ -2,6 +2,14 @@
 
 value=$(<$1)
 
+mkdir "repos"
+cd "repos"
+
 while read -r line; do
+  arrIn=(${line//// })
+  name=${arrIn[2]}
+  mkdir $name
+  cd $name
   git clone $line
+  cd ..
 done <<< "$value"
